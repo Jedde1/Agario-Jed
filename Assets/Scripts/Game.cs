1384a40f8ab1443cac65815d9d0f22026efee550
Jedde1 <54158726+Jedde1@users.noreply.github.com>
2019-10-16 15:53:29 +1100

    initial

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    #region Singleton
+    public static Game Instance = null;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    public Camera cam; // Reference to the Game Camera
+    public Renderer background;
+
+    public Vector3 GetAdjustedPosition(Vector3 incomingPos)
+    {
+        // Calculate Half Size
+        Vector3 size = background.bounds.size;
+        Vector3 halfSize = size * .5f;
+
+        // Outside Right
+        if (incomingPos.x > halfSize.x)
+            incomingPos.x = halfSize.x;
+        // Outside Left
+        if (incomingPos.x < -halfSize.x)
+            incomingPos.x = -halfSize.x;
+
+        // Outside Top
+        if (incomingPos.y > halfSize.y)
+            incomingPos.y = halfSize.y;
+        // Outside Bottom
+        if (incomingPos.y < -halfSize.y)
+            incomingPos.y = -halfSize.y;
+
+        return incomingPos;
+    }
+
+    public Vector3 GetRandomPosition()
+    {
+        Vector3 size = background.bounds.size;
+        return Random.insideUnitCircle * size;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Manager/FoodManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnDelay = 1f;
+    public int startFoodAvailable = 200;
+    public int maxFoodAvailable = 500;
+    // Start is called before the first frame updat
+
+    private float spawnTimer = 0f;
+    void Start()
+    {
+        for (int i = 0; i < startFoodAvailable; i++)
+        {
+            Spawn();
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if(spawnTimer >= spawnDelay)
+        {
+            Spawn();
+            spawnTimer = 0f;
+        }
+    }
+    
+    public void Spawn()
+    {
+        Vector3 randomPos = Game.Instance.GetRandomPosition();
+
+        Instantiate(prefab, randomPos, Quaternion.identity, transform);
+    }
+}
